0a3d25a1be904f400ce71c130872b3901dee4b34
Canfer Ulaş Şahin <ulssahn@outlook.com>
2024-05-27 04:04:41 +0300

    Add files via upload

---
+++ VariableTypes/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean x = true;
+        char a = 'A';
+        byte b = 1;
+        short c = 1111;
+        int d = 11111111;
+        long e = 1231231232;
+        float g = 0.3f;
+        double f = 0.3;
+    }
+}
