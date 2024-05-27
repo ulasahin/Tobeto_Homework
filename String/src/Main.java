94e4f8018d1f168560f8a33d15bab39f13cc7d25
Canfer Ulaş Şahin <ulssahn@outlook.com>
2024-05-27 03:55:26 +0300

    Update Main.java

--- String/src/Main.java
+++ String/src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         String message = "Bugün hava çok güzel.";
+        int sayi = 10;
+        
+        System.out.println(sayi);
 
         System.out.println(message);
         System.out.println("Eleman sayısı : " + message.length());
